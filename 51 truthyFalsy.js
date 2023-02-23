3b174912e9ff7a966269e4b00f7123f45079af07
Vishal Vansjariya <vishalvansjariya2512@gmail.com>
2023-02-24 03:06:26 +0530

    :fire:

---
+++ 51 truthyFalsy.js
@@ -0,0 +1,18 @@
+// Falsy Value (Only below six are falsy)
+let x = false;
+// let x = 0;
+// let x = "";
+// let x = null;
+// let x = undefined;
+// let x = NaN;
+
+if (x) {
+  console.log("Truthy");
+} else {
+  console.log("Falsy");
+}
+
+// except above six all are truthy
+
+// let x = []; => truthy
+// let x = {}; => truthy
